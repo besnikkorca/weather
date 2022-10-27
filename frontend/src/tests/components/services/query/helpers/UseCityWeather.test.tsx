e7f7a643067c9ff9c35b4e3095f4b0cf07d053af
Besnik Korca <besnik.korca1@gmail.com>
2022-10-27 22:55:58 +0200

    add tests for UseCityWeather

---
+++ frontend/src/tests/components/services/query/helpers/UseCityWeather.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import UseCityWeather from 'src/components/services/query/helpers/UseCityWeather';
+import QueryClientProvider from 'src/components/services/query/QueryClientProvider';
+
+describe('UseCityWeather', () => {
+  it('should render successfully', async () => {
+    const spy = jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue({ city: { name: 'Moscow' } }),
+    } as any);
+
+    const { baseElement } = render(
+      <QueryClientProvider>
+        <UseCityWeather cityId={123}>
+          {(query) => {
+            console.log(`query.isLoading - `, query.isLoading);
+            return <h1>{query.data?.city.name}</h1>;
+          }}
+        </UseCityWeather>
+      </QueryClientProvider>
+    );
+
+    const name = await screen.findByText('Moscow');
+
+    expect(name).toBeInTheDocument();
+    expect(baseElement).toBeTruthy();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+});

60eee0097dfac013c1dacf39b1dcb3d81e4ecc0b
Besnik Korca <besnik.korca1@gmail.com>
2022-10-27 19:11:37 +0200

    implement getCityId

---
+++ frontend/src/utils/getCityId/getCityId.ts
@@ -0,0 +1,26 @@
+import cities from './city.list.min.json';
+
+interface City {
+  id: number;
+  name: string;
+  state: string;
+  country: string;
+  coord: { lon: number; lat: number };
+}
+
+export default function getCityId(name: string, country: string, state?: string) {
+  if (!name) throw Error('City name is required');
+
+  const found = (cities as City[]).find((city) => {
+    const nameMatch = city.name === name;
+    const countryMatch = city.country === country;
+    if (state) {
+      return nameMatch && countryMatch && city.state === state;
+    }
+    return nameMatch && countryMatch;
+  });
+
+  if (!found) throw Error('City not found');
+
+  return found.id;
+}

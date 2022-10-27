e5763fd8ae055e54ad8c9b81d00a5b624081f3c5
Besnik Korca <besnik.korca1@gmail.com>
2022-10-27 19:52:58 +0200

    add tests for UseQuery

---
+++ frontend/src/tests/components/services/query/UseQuery.test.tsx
@@ -0,0 +1,65 @@
+import { render, screen } from '@testing-library/react';
+import UseQuery from 'services/query/UseQuery';
+import * as ReactQuery from 'react-query';
+
+function mockUseQuery(implementation: (...args: any[]) => any) {
+  return jest.spyOn(ReactQuery, 'useQuery').mockImplementation(implementation);
+}
+
+describe('UseQuery', () => {
+  it('should render successfully', () => {
+    const useQuerySpy = mockUseQuery((key, fn, options) => ({
+      data: fn({ queryKey: [], meta: {} }),
+      isLoading: false,
+      isError: false,
+    }));
+
+    const props = {
+      queryKey: ['test'],
+      fn: () => 'Moscow weather',
+      options: {},
+    };
+
+    const { baseElement } = render(
+      <UseQuery<string> {...props}>{(query) => <div>{query.data}</div>}</UseQuery>
+    );
+
+    const child = screen.getByText('Moscow weather');
+
+    expect(child).toBeInTheDocument();
+    expect(baseElement).toBeTruthy();
+    expect(useQuerySpy).toHaveBeenCalled();
+  });
+
+  it('should render error', () => {
+    const useQuerySpy = mockUseQuery((key, fn, options) => ({
+      data: fn({ queryKey: [], meta: {} }),
+      isLoading: false,
+      isError: true,
+    }));
+
+    const props = {
+      queryKey: ['test'],
+      fn: () => {
+        return 'Moscow weather';
+      },
+      options: {},
+    };
+
+    const { baseElement } = render(
+      <UseQuery<string> {...props}>
+        {(query) => (query.isError ? <div>Error occurred</div> : <div>{query.data}</div>)}
+      </UseQuery>
+    );
+
+    const child = screen.getByText('Error occurred');
+
+    expect(child).toBeInTheDocument();
+    expect(baseElement).toBeTruthy();
+    expect(useQuerySpy).toHaveBeenCalled();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+});

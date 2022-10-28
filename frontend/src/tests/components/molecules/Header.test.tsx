2d7cffd2ba52c256b49642881ac7e2899f28e134
Besnik Korca <besnik.korca1@gmail.com>
2022-10-28 11:20:24 +0200

    add Header tests

---
+++ frontend/src/tests/components/molecules/Header.test.tsx
@@ -0,0 +1,37 @@
+import { screen, render } from '@testing-library/react';
+import Header from 'molecules/Header';
+
+describe('Header', () => {
+  it('should render', () => {
+    const view = render(<Header active="Ottawa" setActive={() => {}} />);
+
+    expect(view).toMatchSnapshot();
+  });
+
+  it('renders with Ottawa active', () => {
+    render(<Header active="Ottawa" setActive={() => {}} />);
+
+    expect(screen.getByText('Ottawa')).toHaveClass('active');
+  });
+
+  it('renders with Moscow active', () => {
+    render(<Header active="Moscow" setActive={() => {}} />);
+
+    expect(screen.getByText('Moscow')).toHaveClass('active');
+  });
+
+  it('renders with Tokyo active', () => {
+    render(<Header active="Tokyo" setActive={() => {}} />);
+
+    expect(screen.getByText('Tokyo')).toHaveClass('active');
+  });
+
+  it('should call setActive when clicked', () => {
+    const mockSetActive = jest.fn();
+    render(<Header active="Ottawa" setActive={mockSetActive} />);
+
+    screen.getByText('Moscow').click();
+
+    expect(mockSetActive).toBeCalledTimes(1);
+  });
+});

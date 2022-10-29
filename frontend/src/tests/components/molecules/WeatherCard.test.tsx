fde66e88f67d3f1856a89ffdda3165ba3bb75197
Besnik Korca <besnik.korca1@gmail.com>
2022-10-29 10:06:03 +0200

    update tests for WeatherCard

--- frontend/src/tests/components/molecules/WeatherCard.test.tsx
+++ frontend/src/tests/components/molecules/WeatherCard.test.tsx
@@ -1,7 +1,14 @@
 import { render } from '@testing-library/react';
 import WeatherCard from 'molecules/WeatherCard';
+import WeatherList from 'src/components/molecules/WeatherList';
 
 describe('WeatherCard', () => {
+  it('should render', () => {
+    const { container } = render(<WeatherList />);
+
+    expect(container).toBeInTheDocument();
+  });
+
   it('renders correctly', () => {
     const { container } = render(<WeatherCard />);
 

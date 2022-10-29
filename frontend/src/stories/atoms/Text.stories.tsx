9010983268b951a8aa012e72e5ac562e9b38f4ac
Besnik Korca <besnik.korca1@gmail.com>
2022-10-29 09:58:27 +0200

    update Text stories

--- frontend/src/stories/atoms/Text.stories.tsx
+++ frontend/src/stories/atoms/Text.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Text from 'atoms/Text';
+import { TextSize } from 'src/components/atoms/Text/types';
 
 export default {
   title: 'Atoms/Text',
@@ -11,4 +12,28 @@ const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   children: 'Hello world',
+};
+
+export const XLarge = Template.bind({});
+XLarge.args = {
+  children: 'Hello world',
+  size: TextSize.xLarge,
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  children: 'Hello world',
+  size: TextSize.large,
+};
+
+export const Normal = Template.bind({});
+Normal.args = {
+  children: 'Hello world',
+  size: TextSize.normal,
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  children: 'Hello world',
+  size: TextSize.small,
 };

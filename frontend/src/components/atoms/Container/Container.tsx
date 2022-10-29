1225a2470e083bf73d4ed452c0bcf6e6eca88f98
Besnik Korca <besnik.korca1@gmail.com>
2022-10-29 09:55:41 +0200

    refactor container to align with design

--- frontend/src/components/atoms/Container/Container.tsx
+++ frontend/src/components/atoms/Container/Container.tsx
@@ -4,19 +4,52 @@ import styles from './Container.module.scss';
 import { Props, BG } from './types';
 
 export default class Container extends Component<Props> {
-  static defaultProps = { bg: BG.main, full: false };
+  static defaultProps: Partial<Props> = {
+    bg: BG.main,
+    full: false,
+    flexColumn: false,
+    maxWidth: false,
+    border: false,
+    centerContent: false,
+    centerHorizontal: false,
+    centerVertical: false,
+    borderRadius: false,
+    shadow: false,
+  };
+
+  getClassNames = () => {
+    const {
+      full,
+      flexColumn,
+      bg,
+      maxWidth,
+      centerContent,
+      centerHorizontal,
+      centerVertical,
+      border,
+      borderRadius,
+      shadow,
+    } = this.props;
+    return classNames(styles.container, {
+      [styles.full]: full,
+      [styles.flexColumn]: flexColumn,
+      [styles.mainBG]: bg === BG.main,
+      [styles.transparentBG]: bg === BG.transparent,
+      [styles.maxWidthLarge]: maxWidth,
+      [styles.centerContent]: centerContent,
+      [styles.centerHorizontal]: centerHorizontal,
+      [styles.centerVertical]: centerVertical,
+      [styles.border]: border,
+      [styles.borderRadius]: borderRadius,
+      [styles.shadow]: shadow,
+    });
+  };
+
   render() {
+    const { style, children } = this.props;
     return (
-      <div
-        className={classNames(styles.container, {
-          [styles.full]: this.props.full,
-          [styles.bg]: this.props.bg,
-          [styles.maxWidthLarge]: this.props.maxWidth,
-          [styles.centerContent]: this.props.centerContent,
-          [styles.border]: this.props.border,
-        })}
-      >
-        {this.props.children}
+      <div className={this.getClassNames()} style={style}>
+        {children}
       </div>
     );
   }
--- frontend/src/components/atoms/Container/types.ts
+++ frontend/src/components/atoms/Container/types.ts
@@ -1,11 +1,18 @@
 export enum BG {
+  transparent = 'transparent',
   main = 'main',
 }
 export type Props = {
   children?: React.ReactNode;
+  flexColumn?: boolean;
   full?: boolean;
   bg: BG;
   maxWidth?: boolean;
   border?: boolean;
   centerContent?: boolean;
+  centerHorizontal?: boolean;
+  centerVertical?: boolean;
+  style?: React.CSSProperties | undefined;
+  borderRadius?: boolean;
+  shadow?: boolean;
 };

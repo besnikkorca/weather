1b5b936ebd22de2c893a410e63df550df90df815
Besnik Korca <besnik.korca1@gmail.com>
2022-10-29 09:56:13 +0200

    modify Text to align with design

--- frontend/src/components/atoms/Text/Text.tsx
+++ frontend/src/components/atoms/Text/Text.tsx
@@ -1,11 +1,33 @@
 import classNames from 'classnames';
 import { Component } from 'react';
 import styles from './Text.module.scss';
-import { Props } from './types';
+import { Props, TextFamily, TextSize, TextWeight } from './types';
 
 export default class Text extends Component<Props> {
-  static defaultProps = {};
+  static defaultProps: Partial<Props> = {
+    size: TextSize.normal,
+    weight: TextWeight.normal,
+    noMargin: false,
+  };
+
   render() {
-    return <p className={classNames(styles.default)}>{this.props.children}</p>;
+    const { noMargin, size, weight, family, style } = this.props;
+    return (
+      <p
+        className={classNames(styles.default, {
+          [styles.noMargin]: noMargin,
+          [styles.small]: size === TextSize.small,
+          [styles.normal]: size === TextSize.normal,
+          [styles.large]: size === TextSize.large,
+          [styles.xLarge]: size === TextSize.xLarge,
+          [styles.weightNormal]: weight === TextWeight.normal,
+          [styles.bold]: weight === TextWeight.bold,
+          [styles.teko]: family === TextFamily.teko,
+        })}
+        style={style}
+      >
+        {this.props.children}
+      </p>
+    );
   }
 }
--- frontend/src/components/atoms/Text/types.ts
+++ frontend/src/components/atoms/Text/types.ts
@@ -1,3 +1,25 @@
+import React from 'react';
+
+export enum TextFamily {
+  teko = 'teko',
+}
+
+export enum TextWeight {
+  normal = 'normal',
+  bold = 'bold',
+}
+
+export enum TextSize {
+  small = 'small',
+  normal = 'normal',
+  large = 'large',
+  xLarge = 'xLarge',
+}
 export interface Props {
+  noMargin?: boolean;
   children: React.ReactNode;
+  size?: TextSize;
+  weight?: TextWeight;
+  family?: TextFamily | undefined;
+  style?: React.CSSProperties | undefined;
 }
